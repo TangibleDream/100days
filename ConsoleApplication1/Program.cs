2468bf1aafc0ac6fec64db55234836c00ce470ce
TangibleDream <darclan28@yahoo.com>
2017-12-30 18:13:11 -0600

    dayCalculator added

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dayCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysLeft;
+            Console.WriteLine("How many days remain?: ");
+            daysLeft = Convert.ToInt32(Console.ReadLine());
+            DateTime endDate = DateTime.Now.AddDays(daysLeft);
+            Console.WriteLine($"Your challenge will end on {endDate}");
+            Thread.Sleep(10000);
+        }
+    }
+}
